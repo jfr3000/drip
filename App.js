4cd0984f0b4ea1dcdb95a258f6b96d8c456d6ced
emelko <ml.kochsiek@mailbox.org>
2018-05-30 12:33:19 +0200

    Set home as main component, rename list

--- App.js
+++ App.js
@@ -1,59 +1,21 @@
 import React, { Component } from 'react'
 import {
   View,
-  Text,
-  Button,
-  TextInput,
-  FlatList
+  Button
 } from 'react-native'
 import * as styles from './styles'
-import Datastore from 'react-native-local-mongodb'
 
-const db = new Datastore({ filename: 'asyncStorageKey', autoload: true })
-
-export default class drip extends Component {
+export default class home extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      temperatures: []
-    }
-    db.find({ key: { $exists: true } }, (err, persistedTemperatures) => {
-      if (err) throw err
-      this.setState({
-        temperatures: [...persistedTemperatures, ...this.state.temperatures]
-      })
-    })
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <TextInput
-          placeholder="Enter your temperature"
-          onChangeText={(val) => {
-            this.setState({currentValue: val})
-          }}
-        />
         <Button
-          onPress={() => {
-            const newTemp = {
-              value: this.state.currentValue,
-              key: Date.now().toString()
-            }
-            this.setState({
-              temperatures: [newTemp, ...this.state.temperatures]
-            })
-            db.insert(newTemp, (err) => {
-              if (err) console.log(err)
-            })
-          }}
-          title="Save"
-        />
-        <FlatList
-          data = {this.state.temperatures}
-          extraData = {this.state}
-          renderItem={({item}) => <Text>{item.value}</Text>}
-        />
+          onPress={() => {}}
+          title="Home"></Button>
       </View>
     )
   }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native'
-import App from './App'
+import Home from './App'
 
-AppRegistry.registerComponent('drip', () => App)
+AppRegistry.registerComponent('home', () => Home)
