0179c4588ed957c608cf9890ca7e8bb99638baf8
Julia Friesel <julia.friesel@gmail.com>
2018-07-10 10:56:33 +0200

    Detect pre- and postovulatory phase with no previous higher measurement

--- test/sympto/index.spec.js
+++ test/sympto/index.spec.js
@@ -11,48 +11,117 @@ function convertToSymptoFormat(val, i) {
   return sympto
 }
 
+const cycleWithTempShift = [36.6, 36.6, 36.6, 36.6, 36.6, 36.6, 36.8, 36.8, 36.8]
+  .map(num => ({ temperature: num }))
+  .map(convertToSymptoFormat)
+
+const cycleWithoutTempShift = [36.6, 36.6, 36.6, 36.6, 36.6, 36.6, 36.8, 36.8]
+  .map(num => ({ temperature: num }))
+  .map(convertToSymptoFormat)
+
+const cycleWithTempAndMucusShift = [
+  { temperature: 36.6, bleeding: 2 },
+  { temperature: 36.65 },
+  { temperature: 36.5 },
+  { temperature: 36.6 },
+  { temperature: 36.55 },
+  { temperature: 36.7, mucus: 0 },
+  { temperature: 36.75, mucus: 0 },
+  { temperature: 36.45, mucus: 1 },
+  { temperature: 36.5, mucus: 4 },
+  { temperature: 36.4, mucus: 2 },
+  { temperature: 36.5, mucus: 3 },
+  { temperature: 36.55, mucus: 3 },
+  { temperature: 36.45, mucus: 3 },
+  { temperature: 36.5, mucus: 4 },
+  { temperature: 36.55, mucus: 4 },
+  { temperature: 36.7, mucus: 3 },
+  { temperature: 36.65, mucus: 3 },
+  { temperature: 36.75, mucus: 4 },
+  { temperature: 36.8, mucus: 1 },
+  { temperature: 36.85, mucus: 2 },
+  { temperature: 36.8, mucus: 2 },
+  { temperature: 36.9, mucus: 2 },
+  { temperature: 36.9, mucus: 1 },
+  { temperature: 36.85, mucus: 1 },
+  { temperature: 36.9, mucus: 1 },
+  { temperature: 36.8, mucus: 1 },
+  { temperature: 36.9, mucus: 1 }
+].map(convertToSymptoFormat)
+
 describe('sympto', () => {
   describe('evaluating mucus and temperature shift together', () => {
-    it('reports fertile when mucus reaches best quality again within temperature evaluation phase', function () {
-      const values = [
-        { temperature: 36.6 , bleeding: 2 },
-        { temperature: 36.65 },
-        { temperature: 36.5 },
-        { temperature: 36.6 },
-        { temperature: 36.55 },
-        { temperature: 36.7, mucus: 0 },
-        { temperature: 36.75, mucus: 0 },
-        { temperature: 36.45, mucus: 1 },
-        { temperature: 36.5, mucus: 4 },
-        { temperature: 36.4, mucus: 2 },
-        { temperature: 36.5, mucus: 3 },
-        { temperature: 36.55, mucus: 3 },
-        { temperature: 36.45, mucus: 3 },
-        { temperature: 36.5, mucus: 4 },
-        { temperature: 36.55, mucus: 4 },
-        { temperature: 36.7, mucus: 3 },
-        { temperature: 36.65, mucus: 3 },
-        { temperature: 36.75, mucus: 4 },
-        { temperature: 36.8, mucus: 1 },
-        { temperature: 36.85, mucus: 2 },
-        { temperature: 36.8, mucus: 2 },
-        { temperature: 36.9, mucus: 2 },
-        { temperature: 36.9, mucus: 1 },
-        { temperature: 36.85, mucus: 1 },
-        { temperature: 36.9, mucus: 1 },
-        { temperature: 36.8, mucus: 1 },
-        { temperature: 36.9, mucus: 1 }
-      ]
+    describe('with no previous higher measurement', () => {
+      it('with no shifts detects only periovulatory', function () {
+        const values = [
+          { temperature: 36.6, bleeding: 2 },
+          { temperature: 36.65 },
+          { temperature: 36.5 },
+          { temperature: 36.6 },
+          { temperature: 36.55 },
+          { temperature: 36.7, mucus: 0 },
+          { temperature: 36.75, mucus: 0 },
+          { temperature: 36.45, mucus: 1 }
+        ]
 
-      const temperatures = values.map(convertToSymptoFormat)
+        const cycle = values.map(convertToSymptoFormat)
+        const status = getSensiplanStatus({
+          cycle,
+          previousCycle: cycleWithoutTempShift
+        })
+
+        expect(status).to.eql({
+          assumeFertility: true,
+          phases: {
+            periOvulatory: {
+              start: {
+                date: 0,
+                time: '00:00'
+              },
+              cycleDays: cycle
+            }
+          },
+        })
+      })
+
+      it('with shifts detects only periovulatory and postovulatory', function () {
+        const status = getSensiplanStatus({
+          cycle: cycleWithTempAndMucusShift,
+          previousCycle: cycleWithoutTempShift
+        })
+
+        expect(status.temperatureShift).to.be.an('object')
+        expect(status.mucusShift).to.be.an('object')
+        expect(status.assumeFertility).to.be.true()
+        expect(Object.keys(status.phases)).to.eql(['periOvulatory', 'postOvulatory'])
+        expect(status.phases.periOvulatory).to.eql({
+          start: {
+            date: 0,
+            time: '00:00'
+          },
+          cycleDays: cycleWithTempAndMucusShift.slice(0, 21)
+        })
+        expect(status.phases.postOvulatory).to.eql({
+          start: {
+            date: 20,
+            time: '18:00'
+          },
+          cycleDays: cycleWithTempAndMucusShift.slice(20)
+        })
+      })
+    })
+  })
+  describe('with shifts', () => {
+    it.skip('reports fertile when mucus reaches best quality again within temperature evaluation phase', function () {
       const status = getSensiplanStatus(temperatures)
       expect(status).to.eql({
         assumeFertility: false,
-        phases: [
-          { startDate: 0, startTime: '00:00'},
-          'TODO',
-          { startDate: 17, startTime: '18:00'}
-        ],
+        phases: {
+          preOvulatory:
+            { startDate: 0, startTime: '00:00' },
+          periOvulatory: 'TODO',
+          postOvulatory: { startDate: 17, startTime: '18:00' }
+        },
         temperatureShift: {
           detected: true,
           ltl: 36.55,
@@ -78,5 +147,6 @@ describe('sympto', () => {
         }
       })
     })
+
   })
 })
