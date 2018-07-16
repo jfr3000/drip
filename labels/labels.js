b455a030cc6abbbb4e48b60a7a799c5befe8d01d
Julia Friesel <julia.friesel@gmail.com>
2018-07-16 18:39:40 +0200

    Label last day of post-ovu phase differently

--- labels/labels.js
+++ labels/labels.js
@@ -3,5 +3,6 @@ export const bleeding = ['spotting', 'light', 'medium', 'heavy']
 export const fertilityStatus = {
   fertile: 'fertile',
   infertile: 'infertile',
+  fertileUntilEvening: 'Fertile phase ends in the evening',
   unknown: 'We cannot show any cycle information because no menses has been entered'
 }
