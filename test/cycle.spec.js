42a96da69d9444f7cb38d7a65640f59cae045093
Julia Friesel <julia.friesel@gmail.com>
2018-07-18 13:32:40 +0200

    Change cycle module API, add getCycleForDay, remove unused functions

--- test/cycle.spec.js
+++ test/cycle.spec.js
@@ -144,7 +144,7 @@ describe('getCycleDay', () => {
   })
 })
 
-describe('getPreviousCycles', () => {
+describe('getCyclesBefore', () => {
   it('gets previous cycles', () => {
     const cycleDaysSortedByDate = [
       {
@@ -185,11 +185,11 @@ describe('getPreviousCycles', () => {
       },
     ]
 
-    const { getPreviousCycles } = cycleModule({
+    const { getCyclesBefore } = cycleModule({
       cycleDaysSortedByDate,
       bleedingDaysSortedByDate: cycleDaysSortedByDate.filter(d => d.bleeding)
     })
-    const result = getPreviousCycles(cycleDaysSortedByDate[0])
+    const result = getCyclesBefore(cycleDaysSortedByDate[0])
     expect(result.length).to.eql(3)
     expect(result).to.eql([
       [
@@ -228,6 +228,109 @@ describe('getPreviousCycles', () => {
           bleeding: { value: 2 }
         },
       ]
+    ])
+  })
+})
+
+describe('getCycleForDay', () => {
+  const cycleDaysSortedByDate = [
+    {
+      date: '2018-07-05',
+      bleeding: { value: 2 }
+    },
+    {
+      date: '2018-06-05',
+      bleeding: { value: 2 }
+    },
+    {
+      date: '2018-05-05',
+      mucus: { value: 2 }
+    },
+    {
+      date: '2018-05-04',
+      bleeding: { value: 2 }
+    },
+    {
+      date: '2018-05-03',
+      bleeding: { value: 2 }
+    },
+    {
+      date: '2018-04-05',
+      mucus: { value: 2 }
+    },
+    {
+      date: '2018-04-04',
+      mucus: { value: 2 }
+    },
+    {
+      date: '2018-04-03',
+      mucus: { value: 2 }
+    },
+    {
+      date: '2018-04-02',
+      bleeding: { value: 2 }
+    },
+  ]
+  const { getCycleForDay } = cycleModule({
+    cycleDaysSortedByDate,
+    bleedingDaysSortedByDate: cycleDaysSortedByDate.filter(d => d.bleeding)
+  })
+
+  it('gets cycle that has only one day', () => {
+    const result = getCycleForDay('2018-07-05')
+    expect(result.length).to.eql(1)
+    expect(result).to.eql([
+      {
+        date: '2018-07-05',
+        bleeding: { value: 2 }
+      }
+    ])
+    const result2 = getCycleForDay('2018-06-05')
+    expect(result2.length).to.eql(1)
+    expect(result2).to.eql([
+      {
+        date: '2018-06-05',
+        bleeding: { value: 2 }
+      }
+    ])
+  })
+
+  it('for later date gets cycle that has only one day', () => {
+    const result = getCycleForDay('2018-06-20')
+    expect(result.length).to.eql(1)
+    expect(result).to.eql([
+      {
+        date: '2018-06-05',
+        bleeding: { value: 2 }
+      }
+    ])
+  })
+
+  it('returns null if there is no cycle start for that date', () => {
+    const result = getCycleForDay('2018-04-01')
+    expect(result).to.eql(null)
+  })
+
+  it('gets cycle for day', () => {
+    const result = getCycleForDay('2018-04-04')
+    expect(result.length).to.eql(4)
+    expect(result).to.eql([
+      {
+        date: '2018-04-05',
+        mucus: { value: 2 }
+      },
+      {
+        date: '2018-04-04',
+        mucus: { value: 2 }
+      },
+      {
+        date: '2018-04-03',
+        mucus: { value: 2 }
+      },
+      {
+        date: '2018-04-02',
+        bleeding: { value: 2 }
+      },
     ])
   })
 })
