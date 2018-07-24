a2400983cf6d75bcb86423503d64bd343f2e3dd3
Julia Friesel <julia.friesel@gmail.com>
2018-07-24 14:30:20 +0200

    Move status labels to cycle day labels

--- labels/labels.js
+++
@@ -1,8 +0,0 @@
-export const bleeding = ['spotting', 'light', 'medium', 'heavy']
-
-export const fertilityStatus = {
-  fertile: 'fertile',
-  infertile: 'infertile',
-  fertileUntilEvening: 'Fertile phase ends in the evening',
-  unknown: 'We cannot show any cycle information because no menses has been entered'
-}
