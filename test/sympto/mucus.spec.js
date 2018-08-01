8bbf122d12037c1cacb673e2c13ad8a080deb6ba
Julia Friesel <julia.friesel@gmail.com>
2018-08-01 16:21:50 +0200

    Ignore 0 -> 1 mucus shift

--- test/sympto/mucus.spec.js
+++ test/sympto/mucus.spec.js
@@ -14,7 +14,7 @@ describe('sympto', () => {
   describe('detect mucus shift', () => {
     describe('regular rule', () => {
       it('detects mucus shift correctly', function () {
-        const values = [0, 0, 0, 1, 1, 2, 2, 2, 3, 3, 3, 2, 2, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+        const values = [0,0,0,1,1,2,2,2,3,3,3,2,2,0,1,1,1,1,0,0,0,0,0]
           .map(turnIntoCycleDayObject)
         const status = getMucusStatus(values, 12)
         expect(status).to.eql({
@@ -38,7 +38,10 @@ describe('sympto', () => {
       })
 
       it('detects no mucus shift when there are no mucus values', function () {
-        const status = getMucusStatus(Array(10).fill({date: 1, temperature: { value: 35}}))
+        const status = getMucusStatus(Array(10).fill({
+          date: 1,
+          temperature: { value: 35 }
+        }))
         expect(status).to.eql({ detected: false })
       })
 
@@ -47,6 +50,30 @@ describe('sympto', () => {
           .map(turnIntoCycleDayObject)
         const status = getMucusStatus(values, 30)
         expect(status).to.eql({ detected: false })
+      })
+
+      it('detects no mucus shift when mucus only changes from dry to nothing', function () {
+        const values = [0,0,0,1,0,0,0,0,0,0,0]
+          .map(turnIntoCycleDayObject)
+        const status = getMucusStatus(values, 30)
+        expect(status).to.eql({ detected: false })
+      })
+
+      it('ignores an early seeming shift from 0 to 1', function () {
+        const values = [0,0,0,1,0,0,0,2,3,3,3,2,2,0,1,1,1,1,0,0,0,0,0]
+          .map(turnIntoCycleDayObject)
+        const status = getMucusStatus(values, 12)
+        expect(status).to.eql({
+          detected: true,
+          mucusPeak: {
+            date: 10,
+            mucus: { value: 3 }
+          },
+          evaluationCompleteDay: {
+            date: 13,
+            mucus: { value: 0 }
+          }
+        })
       })
     })
   })
