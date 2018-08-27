e64b46c41b116dc2a5b2643aadd2b99a2d68f8ab
Julia Friesel <julia.friesel@gmail.com>
2018-08-27 12:38:17 +0200

    Revert "Install dep"
    This reverts commit 138ce17ade64dcfec10fecab9b1b41e298c35fb2.

--- android/app/src/main/java/com/drip/MainApplication.java
+++ android/app/src/main/java/com/drip/MainApplication.java
@@ -3,7 +3,6 @@ package com.drip;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.wix.reactnativenotifications.RNNotificationsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.rnfs.RNFSPackage;
 import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
@@ -30,7 +29,6 @@ public class MainApplication extends Application implements ReactApplication, Sh
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNNotificationsPackage(MainApplication.this),
             new VectorIconsPackage(),
             new RNFSPackage(),
             new ReactNativeDocumentPicker(),
