a0ef9fbe8e709ab746e09893abc222ba8213ede5
Julia Friesel <julia.friesel@gmail.com>
2018-12-21 16:02:18 +0100

    Add margin

--- components/settings/about.js
+++ components/settings/about.js
@@ -11,7 +11,7 @@ export default class AboutSection extends Component {
           <AppText style={styles.settingsSegmentTitle}>{`${labels.aboutSection.title} `}</AppText>
           <AppText>{`${labels.aboutSection.segmentExplainer} `}</AppText>
         </View>
-        <View style={styles.settingsSegment}>
+        <View style={[styles.settingsSegment, styles.settingsSegmentLast]}>
           <AppText style={styles.settingsSegmentTitle}>{`${labels.credits.title} `}</AppText>
           <AppText>{`${labels.credits.note}`}</AppText>
         </View>
