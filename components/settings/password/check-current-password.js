4e74dd9a5adea4ced0949a8c7932634dfce29b67
mashazyu <mariya.z@gmail.com>
2019-01-05 11:25:43 +0100

    Password verification bug fix

--- components/settings/password/check-current-password.js
+++ components/settings/password/check-current-password.js
@@ -3,18 +3,21 @@ import { openDb } from '../../../db'
 import { shared } from '../../../i18n/en/labels'
 
 export default async function checkPassword({hash, onCancel, onTryAgain }) {
-  const connected = await openDb(hash)
-  if (connected) return true
-  Alert.alert(
-    shared.incorrectPassword,
-    shared.incorrectPasswordMessage,
-    [{
-      text: shared.cancel,
-      onPress: onCancel
-    }, {
-      text: shared.tryAgain,
-      onPress: onTryAgain
-    }]
-  )
-  return false
+  try {
+    await openDb(hash)
+    return true
+  } catch (err) {
+    Alert.alert(
+      shared.incorrectPassword,
+      shared.incorrectPasswordMessage,
+      [{
+        text: shared.cancel,
+        onPress: onCancel
+      }, {
+        text: shared.tryAgain,
+        onPress: onTryAgain
+      }]
+    )
+    return false
+  }
 }
