aadc36f45c48367fa69ffcf3ddac4d72676909e8
emelko <ml.kochsiek@mailbox.org>
2019-05-26 12:58:29 +0200

    Get rid of extra styling for non functional info icon

--- components/settings/nfp-settings/index.js
+++ components/settings/nfp-settings/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import {
   ScrollView, View
 } from 'react-native'
-import styles, { iconStyles } from '../../../styles'
+import styles from '../../../styles'
 import labels from '../../../i18n/en/settings'
 import AppText from '../../app-text'
 import FramedSegment from '../../framed-segment'
@@ -28,10 +28,7 @@ export default class Settings extends Component {
         </FramedSegment>
         <FramedSegment style={styles.framedSegmentLast} >
           <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Icon
-              name="info-with-circle"
-              style={iconStyles.info}
-            />
+            <Icon name="info-with-circle"/>
             <AppText style={styles.framedSegmentTitle}>{` ${labels.preOvu.title} `}</AppText>
           </View>
           <AppText>{labels.preOvu.note}</AppText>
