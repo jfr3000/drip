670857f5ff49059bd3b57d06a3eec7042e3926c9
BloodyMarie <bloodymarie@mailbox.org>
2022-06-19 16:41:52 +0200

    Rename drip to drip.

--- i18n/en/symptom-info.js
+++ i18n/en/symptom-info.js
@@ -11,10 +11,10 @@ export const generalInfo = {
   3. and menstrual bleeding
 the app helps you identify in which phase of the menstrual cycle you are.
 
-drip makes period predictions for you and helps you apply NFP fertility awareness rules. But please remember that this app is made by humans, and humans make mistakes. Always think for yourself: "Does this make sense?" Remember, you don't need an app to understand your cycle! However, drip wants to support you and make period tracking easier, more transparent and secure.
+drip. makes period predictions for you and helps you apply NFP fertility awareness rules. But please remember that this app is made by humans, and humans make mistakes. Always think for yourself: "Does this make sense?" Remember, you don't need an app to understand your cycle! However, drip. wants to support you and make period tracking easier, more transparent and secure.
 
 Please find more info on the sympto-thermal method in ${links.wiki.url}.`,
-  noNfpSymptom: `The app allows you to track this symptom for your information, it is not taken into account for any calculation. On the chart you can check how often you track this symptom.`
+  noNfpSymptom: `The app allows you to track this symptom for your information, it is not taken into account for any calculation. On the chart you can check how often you track this symptom.`,
 }
 
 export default {
@@ -22,7 +22,7 @@ export default {
     title: `Tracking menstrual bleeding`,
     text: `Tracking menstrual bleeding allows you to know the beginning and the end of a menstrual cycle.
 
-After tracking at least 3 menstrual cycles, drip will give you an overview of
+After tracking at least 3 menstrual cycles, drip. will give you an overview of
 · how long your cycles last on average (in "stats"),
 · whether the length of your cycles varied significantly (in "stats" and in bleeding predictions)
 · and predict your next 3 cycles with a range of 3 or 5 days (on home screen and "calendar").
@@ -33,7 +33,7 @@ Excluding bleeding values is for tracking bleeding when it's not marking the sta
 
 ${generalInfo.nfpTfyReminder}`,
   },
-  cervix:  {
+  cervix: {
     title: `Tracking your cervix`,
     text: `The cervix is located inside of the body at the end of the vaginal canal, between the vagina and the uterus.
 
@@ -42,15 +42,15 @@ Tracking how open or closed and how firm or soft the cervix feels can help deter
 By default, the secondary symptom the app uses for NFP evaluation is cervical mucus, but you can change it to cervix in "Settings" -> "NFP Settings".
 
 · How to identify a fertile cervix?
-A fertile cervix is open and feels soft like your earlobes. In contrast, an infertile cervix feels closed and hard, like the tip of your nose. If the cervix feels anything other than closed and hard, drip takes it as a sign of fertility. On the chart, a fertile cervix is colored in dark yellow, and infertile cervix is colored in light yellow.
+A fertile cervix is open and feels soft like your earlobes. In contrast, an infertile cervix feels closed and hard, like the tip of your nose. If the cervix feels anything other than closed and hard, drip. takes it as a sign of fertility. On the chart, a fertile cervix is colored in dark yellow, and infertile cervix is colored in light yellow.
 
 ${generalInfo.chartNfp}
 
 ${generalInfo.excludeExplainer}
 
-${generalInfo.nfpTfyReminder}`
+${generalInfo.nfpTfyReminder}`,
   },
-  desire:  {
+  desire: {
     title: 'Tracking sexual desire',
     text: `The app allows you to track sexual desire independently from sexual activity.
 
@@ -58,9 +58,9 @@ ${generalInfo.cycleRelation}
 
 ${generalInfo.noNfpSymptom}
 
-${generalInfo.curiousNfp}`
+${generalInfo.curiousNfp}`,
   },
-  mood:  {
+  mood: {
     title: 'Tracking mood',
     text: `The app allows you to track your mood.
 
@@ -68,16 +68,16 @@ ${generalInfo.cycleRelation}
 
 ${generalInfo.noNfpSymptom}
 
-${generalInfo.curiousNfp}`
+${generalInfo.curiousNfp}`,
   },
-  mucus:  {
+  mucus: {
     title: 'Tracking cervical mucus',
     text: `Cervical mucus can help determine in which phase of the menstrual cycle you are.
 
 By default the secondary symptom the app uses for NFP evaluation is cervical mucus.
 
 · How to identify fertile cervical mucus?
-Tracking the feeling and the texture of your cervical mucus on a daily basis helps you identify changes of the quality of the cervical mucus. The values you enter for both feeling and texture of your cervical mucus are combined by drip into one of five NFP-conforming values.
+Tracking the feeling and the texture of your cervical mucus on a daily basis helps you identify changes of the quality of the cervical mucus. The values you enter for both feeling and texture of your cervical mucus are combined by drip. into one of five NFP-conforming values.
 From lowest to best quality:
 · t = (dry feeling + no texture),
 · ∅ = (no feeling + no texture),
@@ -87,23 +87,23 @@ From lowest to best quality:
 
 On the chart, cervical mucus is colored in blue: the darker the shade of blue the better the quality of your cervical mucus.
 
-Please note that drip does not yet support "parenthesis values": According to NFP rules, you can qualify a cervical mucus value by putting parentheses around it, to indicate that it doesn't fully meet the descriptors of one of the five categories, and instead is in between. This functionality will be supported in the future.
+Please note that drip. does not yet support "parenthesis values": According to NFP rules, you can qualify a cervical mucus value by putting parentheses around it, to indicate that it doesn't fully meet the descriptors of one of the five categories, and instead is in between. This functionality will be supported in the future.
 
 ${generalInfo.chartNfp}
 
 ${generalInfo.excludeExplainer}
 
-${generalInfo.nfpTfyReminder}`
+${generalInfo.nfpTfyReminder}`,
   },
-  note:  {
+  note: {
     title: 'Notes',
     text: `Note allows you to track any extra information you want to save. It is the only category that can store information for a date in the future. This can be helpful for reminding you of an appointment.
 
 ${generalInfo.noNfpSymptom}
 
-${generalInfo.curiousNfp}`
+${generalInfo.curiousNfp}`,
   },
-  pain:  {
+  pain: {
     title: 'Tracking pain',
     text: `The app allows you to keep track of different kinds of pain you experience.
 
@@ -111,17 +111,17 @@ ${generalInfo.cycleRelation}
 
 ${generalInfo.noNfpSymptom}
 
-${generalInfo.curiousNfp}`
+${generalInfo.curiousNfp}`,
   },
-  sex:  {
+  sex: {
     title: 'Tracking sex and contraceptives',
     text: `The app allows you to track sex independently from sexual desire. You can differentiate between masturbation and sex with a partner/partners. Here you can also track your contraceptive method(s). Only sexual activity will be shown in the "chart" section, lighter purple indicating solo sex and darker purple partner sex. Did you know that having an orgasm can help release cramps?
 
 ${generalInfo.noNfpSymptom}
 
-${generalInfo.curiousNfp}`
+${generalInfo.curiousNfp}`,
   },
-  temperature:  {
+  temperature: {
     title: 'Tracking body basal temperature',
     text: `One of the body signs you need to track for knowing your fertility status is your body basal temperature. The body temperature changes over the course of a menstrual cycle, it rises after ovulation.
 
@@ -140,6 +140,6 @@ ${generalInfo.chartNfp}
 
 ${generalInfo.excludeExplainer}
 
-${generalInfo.nfpTfyReminder}`
+${generalInfo.nfpTfyReminder}`,
   },
 }
