7a6643cc1a7af760a846a41e02133fd5553dff98
Sofiya Tepikin <sofiya.tepikin@gmail.com>
2022-06-23 21:46:41 +0200

    Add alert before closing

--- db/restart-app/index.ios.js
+++ db/restart-app/index.ios.js
@@ -1,5 +1,11 @@
 // Current version of react-native-restart doesn't work with our ios setup
 // therefore we have a fork and use different libraries on the platforms
 import RNExitApp from 'react-native-exit-app-v2'
+import { Alert } from 'react-native'
 
-export const restartApp = RNExitApp.exitApp
+export const restartApp = () => {
+  Alert.alert('Closing app', 'App is shutting down. Please restart.')
+  setTimeout(() => {
+    RNExitApp.exitApp()
+  }, 3000)
+}
