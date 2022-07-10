71d81904ada043873fb5144a518cc2bc8f479f44
bl00dymarie <marie@heartofcode.org>
2022-07-10 18:06:54 +0200

    Make date format more compact for cycle day

--- components/helpers/format-date.js
+++ components/helpers/format-date.js
@@ -6,19 +6,19 @@ import { general as labels } from '../../i18n/en/cycle-day'
 export default function (date) {
   const today = LocalDate.now()
   const dateToDisplay = LocalDate.parse(date)
-  return today.equals(dateToDisplay) ?
-    labels.today :
-    moment(date).format('MMMM Do YYYY')
+  return today.equals(dateToDisplay)
+    ? labels.today
+    : moment(date).format('MMMM Do YYYY')
 }
 
-export function formatDateForShortText (date) {
+export function formatDateForShortText(date) {
   return moment(date.toString()).format('dddd, MMMM Do')
 }
 
 export function dateToTitle(dateString) {
   const today = LocalDate.now()
   const dateToDisplay = LocalDate.parse(dateString)
-  return today.equals(dateToDisplay) ?
-    labels.today :
-    moment(dateString).format('dddd, Do MMM YYYY')
+  return today.equals(dateToDisplay)
+    ? labels.today
+    : moment(dateString).format('ddd DD. MMM YY')
 }
