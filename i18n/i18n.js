af63daf6043191edbfa1ae02f0faf3c1116da2b5
Sofiya Tepikin <sofiya.tepikin@gmail.com>
2022-07-24 09:33:29 +0000

    Bump i18next from 20.6.1 to 21.8.14

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,28 +1,27 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
 
 // translation files
-import en from './en.json';
+import en from './en.json'
 
 const resources = {
-    en: { translation: en },
-  };
+  en: { translation: en },
+}
 
-
-  i18n
+i18n
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    compatibilityJSON: 'v3', // TODO: migrate json to v4 and afterwards remove it
     resources,
     fallbackLng: 'en',
     debug: true,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
-    }
-  });
+    },
+  })
 
-
-export default i18n;
+export default i18n
