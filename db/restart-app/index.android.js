e9bf86006ee1e16b2d22e53ee05f4945775e046a
Sofiya Tepikin <sofiya.tepikin@gmail.com>
2022-08-10 15:59:27 +0200

    Cleanup

--- db/restart-app/index.android.js
+++
@@ -1,5 +0,0 @@
-// Current version of react-native-restart doesn't work with our ios setup
-// therefore we have a fork and use different libraries on the platforms
-import restart from 'react-native-restart'
-
-export const restartApp = restart.Restart
