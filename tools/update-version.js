137c2672d764f5ee6dffb509c095d5ab943b6dd7
Sofiya Tepikin <sofiya.tepikin@gmail.com>
2022-08-15 19:24:34 +0000

    Chore/retire left pad

--- tools/update-version.js
+++ tools/update-version.js
@@ -5,13 +5,11 @@
 
 const ReactNativeVersion = require('react-native-version')
 const readline = require('readline')
-const leftPad = require('left-pad')
 const path = require('path')
 const fs = require('fs')
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
-
     const currentVersion = JSON.parse(fs.readFileSync('./package.json')).version
 
     const rl = readline.createInterface({
@@ -22,7 +20,8 @@ module.exports = () => {
     function createTodaysVersion(attempt) {
       const today = new Date()
       const yy = today.getFullYear() - 2000 // So it's two digits
-      const mm = leftPad(today.getMonth() + 1, 2, '0')
+      const monthString = (today.getMonth() + 1).toString()
+      const mm = monthString.padStart(2, '0')
       const d = today.getDate()
       if (attempt === 0) {
         return `1.${yy}${mm}.${d}`
@@ -42,28 +41,34 @@ module.exports = () => {
       process.exit(1)
     }
 
-    rl.question('Next version will be `' + nextVersion + '`, okay? y/n ', async yn => {
-      if (yn !== 'y' && yn !== 'Y') {
-        reject('Release cancelled.\n')
-        return
+    rl.question(
+      'Next version will be `' + nextVersion + '`, okay? y/n ',
+      async (yn) => {
+        if (yn !== 'y' && yn !== 'Y') {
+          reject('Release cancelled.\n')
+          return
+        }
+
+        const pkgJSON = JSON.parse(fs.readFileSync('./package.json'))
+        const pkgLockJSON = JSON.parse(fs.readFileSync('./package-lock.json'))
+        pkgJSON.version = nextVersion
+        pkgLockJSON.version = nextVersion
+        fs.writeFileSync('./package.json', JSON.stringify(pkgJSON, null, 2))
+        fs.writeFileSync(
+          './package-lock.json',
+          JSON.stringify(pkgLockJSON, null, 2)
+        )
+
+        await ReactNativeVersion.version(
+          {
+            neverAmend: true,
+            target: 'android',
+          },
+          path.resolve(__dirname, '../')
+        )
+        rl.close()
+        resolve()
       }
-
-      const pkgJSON = JSON.parse(fs.readFileSync('./package.json'))
-      const pkgLockJSON = JSON.parse(fs.readFileSync('./package-lock.json'))
-      pkgJSON.version = nextVersion
-      pkgLockJSON.version = nextVersion
-      fs.writeFileSync('./package.json', JSON.stringify(pkgJSON, null, 2))
-      fs.writeFileSync('./package-lock.json', JSON.stringify(pkgLockJSON, null, 2))
-
-      await ReactNativeVersion.version(
-        {
-          neverAmend: true,
-          target: 'android',
-        },
-        path.resolve(__dirname, '../'),
-      )
-      rl.close()
-      resolve()
-    })
+    )
   })
 }
