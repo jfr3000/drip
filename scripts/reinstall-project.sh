22528ffe1762f6f7b937c0173ecb66764a64264f
Sofiya Tepikin <sofiya.tepikin@gmail.com>
2022-09-10 12:25:54 +0000

    Chore/retire npm

--- scripts/reinstall-project.sh
+++ scripts/reinstall-project.sh
@@ -5,11 +5,8 @@ echo "\x1b[35;01m""Start re-installing dependencies...""\x1b[39;49;00m"
 echo "Remove node_modules..."
 rm -rf node_modules
 
-echo "Verify npm cache..."
-npm cache verify
-
-echo "Npm install..."
-npm install
+echo "Yarn install..."
+yarn
 
 echo "Pods install..."
 cd ios && pod install && cd ..
