7418fc2ccea6e5e62bcfa743a9df99d5dd3bf50f
bl00dymarie <ml.kochsiek@mailbox.org>
2023-12-12 15:18:02 +0100

    Give some marginTop on very first License screen

--- components/settings/License.js
+++ components/settings/License.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useTranslation } from 'react-i18next'
+import { StyleSheet } from 'react-native'
 
 import AppPage from '../common/app-page'
 import AppText from '../common/app-text'
 import AppLink from '../common/AppLink'
 import Segment from '../common/segment'
+import { Spacing } from '../../styles'
 
 const License = ({ children }) => {
   const { t } = useTranslation(null, { keyPrefix: 'hamburgerMenu.license' })
   const currentYear = new Date().getFullYear()
   const link = 'https://www.gnu.org/licenses/gpl-3.0.html'
   return (
-    <AppPage title={t('title')}>
+    <AppPage title={t('title')} contentContainerStyle={styles.contentContainer}>
       <Segment last>
         <AppText>{t('text', { currentYear })}</AppText>
         <AppLink url={link}>{link}</AppLink>
@@ -25,5 +27,11 @@ const License = ({ children }) => {
 License.propTypes = {
   children: PropTypes.node,
 }
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    marginTop: Spacing.large,
+  },
+})
 
 export default License
